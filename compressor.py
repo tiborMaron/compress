cea438099cf2861a02eb8230d4d017a8fc9c0da3
Tibor Maron <tibormaron@gmail.com>
2018-03-21 14:23:03 +0100

    Refactored and extended.

--- compressor.py
+++ compressor.py
@@ -1,7 +1,9 @@
 import sys
+import os
 
-LENGTH_OF_SAMPLE = 500
-OFFSET = 30
+LENGTH_OF_SAMPLE = 750
+MARKER = "<#&#>"
+STARTING_CHAR = '\u0100'
 
 
 def read_file(file_name):
@@ -10,65 +12,82 @@ def read_file(file_name):
     return string
 
 
-def scan_string(string):
-    duplications = []
-    sample_string = string[:LENGTH_OF_SAMPLE]
-    char = ord('\u0080')
+def swap(string, duplications, mode="encode"):
 
-    for i in range(len(sample_string) - OFFSET):
-        for j in range(i + 1, len(sample_string) - OFFSET):
-
-            if sample_string[i] == sample_string[j] and sample_string[i + 1] == sample_string[j + 1]:
-
-                n = 0
-                word = []
-                while sample_string[i + n] == sample_string[j + n]:
-                    word.append(sample_string[i + n])
-                    n += 1
-
-                item = ("".join(word), chr(char))
-                duplications.append(item)
-                char += 1
-
-    # Sort by longest word
-    duplications.sort(key=lambda t: len(t[0]), reverse=True)
-
-    # Print for testing only
-    # for i in range(len(duplications)):
-    #    print('"{0}" = "{1}"'.format(duplications[i][0], duplications[i][1]))
-    # print(duplications)
-
-    return duplications
-
-
-def swap_duplications(string, duplications, mode="encode"):
-    key = []
-    for i in range(len(duplications)):
-
-        idx = string.find(duplications[i][0])
-        while idx != -1:
-            string = string.replace(duplications[i][0], duplications[i][1])
-            if "|{0}={1}".format(duplications[i][0], duplications[i][1]) not in key:
-                key.append("|{0}={1}".format(duplications[i][0], duplications[i][1]))
-            idx = string.find(duplications[i][0])
-
+    # ENCODE MODE
     if mode == "encode":
-        return "".join(key) + "<#&#>" + string
+        final_keys = []
+        for i in range(len(duplications)):
+            while True:
+                string = string.replace(duplications[i][0], duplications[i][1])
+                key = "|{0}={1}".format(duplications[i][0], duplications[i][1])
+
+                if key not in final_keys:
+                    final_keys.append(key)
+
+                pos = string.find(duplications[i][0])
+                if pos == -1:
+                    break
+        return "".join(final_keys) + MARKER + string
+
+    # DECODE MODE
     elif mode == "decode":
+        for i in range(len(duplications)):
+            while True:
+                string = string.replace(duplications[i][0], duplications[i][1])
+                pos = string.find(duplications[i][0])
+
+                if pos == -1:
+                    break
         return string
 
 
-def decode(string):
-    encode_marker = string.find("<#&#>")
-    header = string[1:encode_marker]
-    raw_list = header.split("|")
+def encode(string):
+
+    if string.find(MARKER) != -1:
+        raise ValueError
 
     duplications = []
-    for item in raw_list:
-        temp = item.split("=")
-        duplications.append([temp[1], temp[0]])
+    sample = string[:LENGTH_OF_SAMPLE]
+    char_value = ord(STARTING_CHAR)
 
-    return swap_duplications(string[encode_marker + 5:], duplications, mode="decode")
+    for i in range(len(sample)):
+        for j in range(i + 1, len(sample)):
+
+            try:
+                if sample[i] == sample[j] and sample[i + 1] == sample[j + 1]:
+
+                    n = 0
+                    word = []
+                    while sample[i + n] == sample[j + n]:
+                        word.append(sample[i + n])
+                        n += 1
+
+                    item = ("".join(word), chr(char_value))
+                    duplications.append(item)
+                    char_value += 1
+            except IndexError:
+                pass
+
+    duplications.sort(key=lambda t: len(t[0]), reverse=True)
+    return swap(string, duplications)
+
+
+def decode(string):
+    pos_encode_marker = string.find(MARKER)
+
+    if pos_encode_marker == -1:
+        raise ValueError
+
+    header = string[1:pos_encode_marker]
+    raw_list = header.split("|")
+
+    keys = []
+    for key in raw_list:
+        temp = key.split("=")
+        keys.append([temp[1], temp[0]])
+
+    return swap(string[pos_encode_marker + len(MARKER):], keys, mode="decode")
 
 
 def write_to_file(string):
@@ -78,23 +97,56 @@ def write_to_file(string):
 
 def main():
     if len(sys.argv) != 3:
-        print("Not enough arguments!")
+        print("Incorrect number of arguments!")
         return None
 
-    if sys.argv[1] == "-compress":
-        string = read_file(sys.argv[2])
-        orig_len = len(string)
-        duplications = scan_string(string)
-        string = swap_duplications(string, duplications)
-        new_len = len(string)
-        write_to_file(string)
-        print("Original length: {0}"
-              "\nNew length: {1}\n"
-              "Compression rate: {2:.2f}%".format(orig_len, new_len, 100 - (new_len / orig_len * 100)))
-    elif sys.argv[1] == "-decompress":
-        string = read_file(sys.argv[2])
-        string = decode(string)
-        write_to_file(string)
+    # Encoding the file
+    if sys.argv[1] == "-code":
+        try:
+            original_text_as_string = read_file(sys.argv[2])
+        except FileNotFoundError:
+            print("There is no file called '{0}'!".format(sys.argv[2]))
+            return None
+
+        orig_size = os.path.getsize(sys.argv[2])
+
+        try:
+            encoded_string = encode(original_text_as_string)
+        except ValueError:
+            print("File already encoded!")
+            return None
+
+        write_to_file(encoded_string)
+        new_size = os.path.getsize(sys.argv[2])
+
+        print("Original length: {0}\n"
+              "New length: {1}\n"
+              "Compression rate (by length): -{2:.2f}%\n".format(
+                len(original_text_as_string), len(encoded_string),
+                100 - (len(encoded_string) / len(original_text_as_string) * 100)))
+
+        print("Original size: {0:.2f}KiB\n"
+              "New size: {1:.2f}KiB\n"
+              "Compression rate (by size): -{2:.2f}%".format(
+                orig_size / 1024, new_size / 1024,
+                100 - (new_size / orig_size * 100)))
+
+    # Decoding the file
+    elif sys.argv[1] == "-decode":
+        try:
+            compressed_text_as_string = read_file(sys.argv[2])
+        except FileNotFoundError:
+            print("There is no file called '{0}'!".format(sys.argv[2]))
+            return None
+
+        try:
+            decoded_string = decode(compressed_text_as_string)
+        except ValueError:
+            print("File isn't encoded!")
+            return None
+
+        write_to_file(decoded_string)
+
     else:
         print("Wrong arguments!")
 
